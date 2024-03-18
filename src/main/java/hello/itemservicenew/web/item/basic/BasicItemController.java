a99c9d26865ca87e20e99c531e5a4930cb2bdd38
tj-create <113359042+tj-create@users.noreply.github.com>
2024-03-18 21:05:02 +0900

    item목록 기능 제작

---
+++ src/main/java/hello/itemservicenew/web/item/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.itemservicenew.web.item.basic;
+
+import hello.itemservicenew.domain.item.Item;
+import hello.itemservicenew.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
