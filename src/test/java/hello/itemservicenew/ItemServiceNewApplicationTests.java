721794c703f41503c1a0e19fb1420c85ec8ff1e5
tj-create <113359042+tj-create@users.noreply.github.com>
2024-03-18 13:38:24 +0900

    basic setting

---
+++ src/test/java/hello/itemservicenew/ItemServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
